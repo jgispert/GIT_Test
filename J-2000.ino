3ad3f61871380ec4e3b0aed07ddc72ae2ae003b5
Joan Gispert <joan@gispert.net>
2022-10-24 16:19:54 +0200

    First commit

---
+++ J-2000.ino
@@ -0,0 +1,25 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial J2000 (10,11); // RX,TX
+char llegit;
+
+void setup ()
+{
+  Serial.begin (19200);
+  J2000.begin (9600);
+  //J2000.print(0x0A);
+}
+
+void loop ()
+{
+  if (J2000.available())
+  {
+    //Serial.write ("Inicio");
+    llegit=J2000.read();
+    Serial.write(llegit);
+  }
+  //J2000.print(0x0C);
+  //delay (5000);
+}
+
+
